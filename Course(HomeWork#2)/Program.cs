4c75bd81a9d402ed90d7939dbe9e3535afdcb2dd
user <user@DESKTOP-ML6KUHU>
2023-12-24 21:40:14 +0300

    Добавьте файлы проекта.

---
+++ Course(HomeWork#2)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_HomeWork_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                int digit = 0;
+
+                try
+                {
+                    Console.WriteLine("Какое число Вы хотите проверить на четность?");
+                    digit = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Было введено некоректное число!");
+                    Console.WriteLine("Нажмите Enter чтобы очистить консоль!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                if (digit % 2 == 0)
+                {
+                    Console.WriteLine($"Число {digit} является четным!");
+                }
+                else if (digit % 2 != 0)
+                {
+                    Console.WriteLine($"Число {digit} не четное!");
+                }
+
+                Console.WriteLine("Нажмите Enter чтобы очистить консоль");
+                Console.ReadLine();
+            }
+        }
+    }
+}
